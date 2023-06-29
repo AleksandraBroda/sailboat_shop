9c338e505be678b5f94dc86c89e7705251ff4e15
Aleksandra <aleksandrabrodaa@gmail.com>
2023-06-29 09:59:54 +0200

    modified kod2.js

--- kod2.js
+++ kod2.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end(`<html><body style="background-color: black; color: yellow"><span style="font-size: large"><p style="text-align: center"><h1><u><b>This is HTML</b></u></h1></p></span></body></html>`);
+  res.end(`<html><center><body style="background-color: black; color: yellow"><span style="font-size: xx-large; font-family: Curlz MT"><h1><u><b>👋 Hello! This is HTML</b></u></h1></span></body></center></html>`);
 }
 
 const server = http.createServer(requestListener);
